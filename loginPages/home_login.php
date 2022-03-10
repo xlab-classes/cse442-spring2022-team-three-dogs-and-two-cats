b54f0ae1780d8c4fe58dc27d0387ea4d95f5dc35
qinran6271 <60624760+qinran6271@users.noreply.github.com>
2022-03-10 00:39:56 -0500

    php create

---
+++ loginPages/home_login.php
@@ -0,0 +1,75 @@
+<?php 
+
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+$mysqli = new mysqli("oceanus.cse.buffalo.edu", "johnbudn", "50382208", "cse442_2022_spring_team_n_db");
+
+// $username =$_GET['username'];
+// $password = $_GET['password'];
+// $result = $mysqli->query("SELECT * FROM user where username = '$username' and password = '$password'");
+// if($result->num_rows >0){
+// echo "User validated";
+// $col = $result->fetch_assoc();  
+// if($col["is_professor"] == 0)
+// echo " User is student";
+// else
+// echo " User is Professor";
+// }
+// else
+// echo "User not validated";
+// $mysqli->close();
+$username = "";
+$password = "";
+// $username_err = "";
+// $password_err = "";
+$login_err = "";
+
+if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])){
+    $mysqli = new mysqli("oceanus.cse.buffalo.edu", "johnbudn", "50382208", "cse442_2022_spring_team_n_db");  
+    if (mysqli_connect_errno()) {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        exit();
+    }    
+    // Check if username is empty   
+    if (isset($_POST["username"]) && isseet($_POST["password"])){
+        $username = $_POST["username"]
+        $password = $_POST["password"]
+
+        $result = $mysqli->query("SELECT * FROM user where username = '$username' and password = '$password'");
+
+        if($result->num_rows >0){
+            echo "User validated";
+            $col = $result->fetch_assoc();  
+
+        if($col["is_professor"] == 0){
+            echo " User is student";
+        }
+        else{
+            echo " User is Professor";
+        }
+        else{
+            echo "User not validated";
+            $mysqli->close();
+        }
+
+    }
+    else{
+        $login_err = "invalid username or password"
+    }
+    // if(empty($_POST["username"])){
+    //     $username_err = "Username cannot be empty.";
+    // } else{
+    //     $username = $_POST["username"];
+    //     print $username;
+    // }
+    
+    // // Check if password is empty
+    // if(empty($_POST["password"])){
+    //     $password_err = "Please cannot be empty.";
+    // } else{
+    //     $password = $_POST["password"];
+    //     print $username;
+    // }
+    //   } 
+
+}
---
+++ loginPages/home_login.py
@@ -0,0 +1,14 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(user='xliu95', password='50306778',host='oceanus.cse.buffalo.edu',database='cse442_2022_spring_team_n_db')
+
+mycursor = mydb.cursor()
+ 
+mycursor.execute("SELECT * FROM user;")
+ 
+myresult = mycursor.fetchall()
+ 
+for x in myresult:
+    print(x)
+
+mydb.close()
