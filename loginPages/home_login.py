64ac5b16d7cb3aea557fff6bfa95286d3c2b5eac
Sammy Liu <xliu95@buffalo.edu>
2022-03-10 17:08:59 -0500

    edited python file and fixed js

--- loginPages/home_login.py
+++ loginPages/home_login.py
@@ -1,17 +1,55 @@
 import mysql.connector
 
-def login():
 
-    mydb = mysql.connector.connect(user='qinranwa', password='50307911',host='oceanus.cse.buffalo.edu',database='cse442_2022_spring_team_n_db')
+
+# def main():
+
+#     mydb = mysql.connector.connect(user='xliu95', password='50306778',host='oceanus.cse.buffalo.edu',database='cse442_2022_spring_team_n_db')
+
+#     mycursor = mydb.cursor()
+    
+#     mycursor.execute("SELECT * FROM user;")
+    
+#     myresult = mycursor.fetchall()
+    
+#     for x in myresult:
+#         print(x)
+    
+#     mydb.close()
+
+def login(username,password):
+
+    mydb = mysql.connector.connect(user='xliu95', password='50306778',host='oceanus.cse.buffalo.edu',database='cse442_2022_spring_team_n_db')
 
     mycursor = mydb.cursor()
     
-    mycursor.execute("SELECT * FROM user;")
-    
+    sql= "SELECT is_professor FROM user WHERE username =%s and password=%s"
+    val = (username,password)
+    mycursor.execute(sql,val)
     myresult = mycursor.fetchall()
+    # return False if it is an inviald user
+    if len(myresult) == 0:
+        print("error")
+        mydb.close()
+        return (False,"Student")  
+    else:
+        # returns True and Student if the user is a student
+        if myresult[0][0] == 0:
+            print("student")
+            mydb.close()
+            return (True,"Student")
+        # returns True and Professor if the user is a prof
+        else:
+            print("prof")
+            mydb.close()
+            return (False,"Professor")
+
     
-    for x in myresult:
-        print(x)
 
-    mydb.close()
 
+
+
+# if __name__ == "__main__":
+#     login("DemoStudent","DemoPass")
+#     login("DemoProf", "DemoPass")
+#     login("DemoStudent","WrongPass")
