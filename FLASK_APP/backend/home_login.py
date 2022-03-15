69ea67b61ec7f5d5b33898fdc0759edb402660b1
qinran6271 <60624760+qinran6271@users.noreply.github.com>
2022-03-14 20:27:24 -0400

    add alter in frontend

--- FLASK_APP/backend/home_login.py
+++ FLASK_APP/backend/home_login.py
@@ -4,12 +4,17 @@ from flask import Blueprint
 
 
 home_login = Blueprint('home_login', __name__)
-@home_login.route("/", methods=['POST'])
+@home_login.route("/", methods=['POST','GET'])
 # @cross_origin()
 def login():
     print("hi")
     data = request.get_json()
-    response = jsonify(result="Student")
+    username = data['username']
+    password = data['password']
+    if username == '' or password == '':
+        response = jsonify(result="username or password cannot be empty")
+    else:
+        response = jsonify(result="Student")
     # response.headers.add("Access-Control-Allow-Origin", "*")
     print(data)
     return response
