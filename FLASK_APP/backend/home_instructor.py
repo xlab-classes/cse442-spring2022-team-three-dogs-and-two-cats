10ecc615e5a7a5db281ce0199fa963cc19f086e4
John Budnick <johnbudn@buffalo.edu>
2022-04-07 17:30:18 -0400

    Swtiched match statement to if/else #41

--- FLASK_APP/backend/home_instructor.py
+++ FLASK_APP/backend/home_instructor.py
@@ -20,8 +20,7 @@ def home_inst():
     if token:
         username = check_token(token)
 
-    match request.method:
-        case 'GET':
+        if request.method == 'GET':
             if token:
                 # Get all classes belonging to professor
                 cursor.execute(
@@ -38,53 +37,56 @@ def home_inst():
                             "classCode": curClass[1],
                             "classSize": curClass[2]
                         }
-                        
-                        classList.append(curClassDict) 
-                    response = jsonify(result="Classes found", listOut=classList, userOut=username)
+
+                        classList.append(curClassDict)
+                    response = jsonify(result="Classes found",
+                                       listOut=classList, userOut=username)
                 else:
-                    response = jsonify(result="No classes found", userOut = username)
+                    response = jsonify(
+                        result="No classes found", userOut=username)
 
             else:
                 response = jsonify(result="not logged in")
+        else:
+            if request.method == 'OPTIONS':
+                response = jsonify(result="200")
+                response.headers.add('Access-Control-Allow-Origin', '*')
 
-        case 'OPTIONS':
-            response = jsonify(result="200")
-            response.headers.add('Access-Control-Allow-Origin', '*')
-
-
-        case 'POST':
-            data = request.get_json()
-            class_name = data['classname']
-            class_size = data['classsize']
-
-            if class_name == '':
-                repsonse = jsonify(result="Class name cannot be empty")
             else:
-                if class_size == '':
-                    repsonse = jsonify(result="Class size cannot be empty")
-                else:
-                    # Generate new class code
-                    newClassCode = ''.join(random.choice(
-                        string.ascii_letters) for i in range(8))
+                if request.method == 'POST':
+                    data = request.get_json()
+                    class_name = data['classname']
+                    class_size = data['classsize']
 
-                    cursor.execute(
-                        "SELECT * FROM class WHERE class_code = %s", newClassCode)
-                    result = cursor.fetchone()
-                    # Prevent duplicate class codes
-                    while result:
-                        newClassCode = ''.join(random.choice(
-                            string.ascii_letters) for i in range(8))
-                        cursor.execute(
-                            "SELECT * FROM class WHERE class_code = %s", newClassCode)
-                        result = cursor.fetchone()
+                    if class_name == '':
+                        repsonse = jsonify(result="Class name cannot be empty")
+                    else:
+                        if class_size == '':
+                            repsonse = jsonify(
+                                result="Class size cannot be empty")
+                        else:
+                            # Generate new class code
+                            newClassCode = ''.join(random.choice(
+                                string.ascii_letters) for i in range(8))
 
-                    # Create new class in DB
-                cursor.execute("INSERT INTO class VALUES (%s,%s,%s, %s, %s)",
-                               (newClassCode, class_name, username, class_size, "0"))
-                cursor.connection.commit()
-                response = jsonify(result="Class created",
-                                   class_code=newClassCode)
-                
-    
+                            cursor.execute(
+                                "SELECT * FROM class WHERE class_code = %s", newClassCode)
+                            result = cursor.fetchone()
+                            # Prevent duplicate class codes
+                            while result:
+                                newClassCode = ''.join(random.choice(
+                                    string.ascii_letters) for i in range(8))
+
+                                cursor.execute(
+                                    "SELECT * FROM class WHERE class_code = %s", newClassCode)
+                                result = cursor.fetchone()
+
+                                # Create new class in DB
+                            cursor.execute("INSERT INTO class VALUES (%s,%s,%s, %s, %s)",
+                                           (newClassCode, class_name, username, class_size, "0"))
+                            cursor.connection.commit()
+                            response = jsonify(result="Class created",
+                                               class_code=newClassCode)
+
     cursor.close()
     return response
