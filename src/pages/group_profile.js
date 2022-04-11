152ef4f9a9ec1ffecfc21d811a0654e033bec367
Cinnamun <fyniixmailz@gmail.com>
2022-04-11 13:38:26 -0400

    Check if professor #61

--- src/pages/group_profile.js
+++ src/pages/group_profile.js
@@ -16,12 +16,13 @@ const GroupProfile = () => {
     const group_code = location.state.groupcode
     const classcode = location.state.classcode
 
-    const [isProf, setProf] = useState(true)
+    const [isProf, setProf] = useState(false)
 
 
     const [sectionNum, setNum] = useState('')
     const [groupMems, setMems] = useState([])
     const [desc, setDesc] = useState('')
+    const [descNew, setDescNew] = useState('')
 
 
 
@@ -30,45 +31,54 @@ const GroupProfile = () => {
         console.log("-------------------------")
         console.log("GROUP CODE: " + group_code)
         console.log("CLASS CODE: " + classcode)
-        
 
-        axios.get('http://127.0.0.1:5000/group_profile', {params:{group_code: group_code, classcode: classcode}}).then(
+
+        axios.get('http://127.0.0.1:5000/group_profile', { params: { group_code: group_code, classcode: classcode } }).then(
             response => {
                 console.log(response.data)
                 setNum(response.data.section_id)
                 setMems(response.data.membersList)
                 setDesc(response.data.desc)
+                if (response.data.isProf == 1) {
+                    setProf(true)
+                }
             })
 
-        axios.options('http://127.0.0.1:5000/group_profile')
+        axios.options('http://127.0.0.1:5000/group_profile', { group_code: group_code, desc: descNew })
             .catch(err => { console.log(err) })
     }, [])
 
 
     function editDesc(event) {
         event.preventDefault()
-        window.alert("Implement edit desc")
+        
+        axios.post('http://127.0.0.1:5000/home_student').then(
+            response => {
+                console.log(response.data)
+            })
+        axios.options('http://127.0.0.1:5000/home_student')
+            .catch(err => { console.log(err) })
     }
 
 
     let buttons
     if (isProf == false) {
-        buttons = 
-        <div className='profileButtons'>
-            <button className='b'>Invite</button>
-            <button className='b'>Leave the Group</button>
-        </div>
+        buttons =
+            <div className='profileButtons'>
+                <button className='b'>Invite</button>
+                <button className='b'>Leave the Group</button>
+            </div>
     }
     else if (isProf == true) {
         buttons =
-        <div className='profileButtons'>
-            <button className='b'>Delete Member</button>
-        </div>
+            <div className='profileButtons'>
+                <button className='b'>Delete Member</button>
+            </div>
     }
 
     return (
         <div className={styles['container']}>
-            
+
             {/* navbar */}
             <Helmet>
                 <title>home_student - project</title>
@@ -79,9 +89,9 @@ const GroupProfile = () => {
                     <span>{groupname}</span>
                 </span>
                 <Link to="/home_student" className={styles['navlink']}>
-                <svg viewBox="0 0 1024 1024" className={styles['homebutton']}>
-                    <path d="M512 128c0 0-263.936 227.84-411.435 351.232-8.661 7.851-15.232 19.285-15.232 32.768 0 23.595 19.072 42.667 42.667 42.667h85.333v298.667c0 23.595 19.072 42.667 42.667 42.667h128c23.595 0 42.667-19.115 42.667-42.667v-170.667h170.667v170.667c0 23.552 19.072 42.667 42.667 42.667h128c23.595 0 42.667-19.072 42.667-42.667v-298.667h85.333c23.595 0 42.667-19.072 42.667-42.667 0-13.483-6.571-24.917-16.341-32.768-146.475-123.392-410.325-351.232-410.325-351.232z"></path>
-                </svg>
+                    <svg viewBox="0 0 1024 1024" className={styles['homebutton']}>
+                        <path d="M512 128c0 0-263.936 227.84-411.435 351.232-8.661 7.851-15.232 19.285-15.232 32.768 0 23.595 19.072 42.667 42.667 42.667h85.333v298.667c0 23.595 19.072 42.667 42.667 42.667h128c23.595 0 42.667-19.115 42.667-42.667v-170.667h170.667v170.667c0 23.552 19.072 42.667 42.667 42.667h128c23.595 0 42.667-19.072 42.667-42.667v-298.667h85.333c23.595 0 42.667-19.072 42.667-42.667 0-13.483-6.571-24.917-16.341-32.768-146.475-123.392-410.325-351.232-410.325-351.232z"></path>
+                    </svg>
                 </Link>
                 {/* name dropdown */}
                 <span className={styles['name']}>
@@ -105,7 +115,7 @@ const GroupProfile = () => {
                                 <span>{mem.username}</span>
                                 <span>{mem.email}</span>
                             </div>
-                            )}
+                        )}
                     </div>
                 </div>
                 <div className='row3'>
